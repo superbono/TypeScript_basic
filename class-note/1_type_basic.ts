dbd3e27cf51ea84c09dd62e450793714bc8d055d
superbono <fr.dev.ar@gmail.com>
2022-07-25 20:00:50 +0900

    typescript 선언방식

---
+++ class-note/1_type_basic.ts
@@ -0,0 +1,15 @@
+
+// 자바스크립트 문자열 선언방식
+// let strJS = 'hello';
+
+// 타입스크립트 선언방식
+let strTS: string = 'hello';
+let numTS: number = 30;
+let arrTS: Array<number> = [10,30,50,80];
+let arrTS1: number[] = [20,30,50]
+
+
+// let arrTS: object = {
+//     name: 'John',
+//     age: 40
+// }
